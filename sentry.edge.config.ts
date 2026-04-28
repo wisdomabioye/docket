525229d38a0863b579221ad6286fb34d92dbed6a
wisdomabioye <wisdomabioye@gmail.com>
2026-04-28 19:08:06 +0100

    feat: integrate Sentry for error tracking and monitoring
    - Added global error handling component to capture exceptions using Sentry.
    - Configured Sentry initialization for client-side, server-side, and edge features.
    - Enabled logging and user PII tracking for better error context.
    - Implemented instrumentation for capturing request errors and router transitions.

---
+++ sentry.edge.config.ts
@@ -0,0 +1,20 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://fdcdcd12a17589c23f53804932a41c2d@o4509884645244928.ingest.us.sentry.io/4511298971500544",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://fdcdcd12a17589c23f53804932a41c2d@o4509884645244928.ingest.us.sentry.io/4511298971500544",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
